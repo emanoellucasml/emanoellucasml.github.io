827ad0cadc21e5eeed9ec5106d41ba8b8022c26a
Emanoel Lucas <emanoellucas123456@gmail.com>
2020-04-14 00:35:10 -0300

    forçando atualização do cache a cada acesso à página

--- tempodeaprender/assets/js/comum.js
+++ tempodeaprender/assets/js/comum.js
@@ -1,5 +1,5 @@
 window.onload = function(){
     let barra = document.querySelector('.barra-atualizacao');
-    let texto = "Última atualização em 14/04/2020 às 00:27"
+    let texto = "Última atualização em 14/04/2020, às 00:34"
     barra.appendChild(document.createTextNode(texto))
 }
