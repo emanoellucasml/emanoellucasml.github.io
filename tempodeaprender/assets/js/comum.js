a18b10a78df5c9cf013aa4d803851f3a8574d0db
Emanoel Lucas <emanoellucas123456@gmail.com>
2020-04-23 21:57:59 -0300

    .

--- tempodeaprender/assets/js/comum.js
+++ tempodeaprender/assets/js/comum.js
@@ -1,5 +1,5 @@
 window.onload = function(){
     let barra = document.querySelector('.barra-atualizacao');
-    let texto = "Última atualização em 21/04/2020, às 21:33."
+    let texto = "Última atualização em 23/04/2020, às 21:58."
     barra.appendChild(document.createTextNode(texto))
 }
